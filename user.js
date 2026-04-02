b2db15ad670177b127eac28d62629247e0020441
Ameur <oubetat.1@gmail.com>
2026-04-02 19:48:44 +0100

    Add files via upload

--- user.js
+++ user.js
@@ -2,18 +2,21 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 
-router.get("/", async (req,res)=> res.json(await User.find()));
-
-router.post("/", async (req,res)=>{
-  const user = new User(req.body);
-  await user.save();
-  res.json({message:"✅ User added"});
+router.get("/", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
 });
 
-router.put("/:id/reward", async (req,res)=>{
+router.post("/", async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  res.json({ message: "✅ User added" });
+});
+
+router.put("/:id/reward", async (req, res) => {
   const user = await User.findById(req.params.id);
   user.points += req.body.points;
-  if(user.points > 100) user.level++;
+  if (user.points > 100) user.level++;
   await user.save();
   res.json(user);
 });
