693c9c5e373a57784f5c3cbc09e37921d9828e00
Ameur <oubetat.1@gmail.com>
2026-04-02 21:14:18 +0100

    Delete rewards.js

--- rewards.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const rewardSchema = new mongoose.Schema({
-  name: String,
-  levelRequired: Number,
-  ecoPoints: Number
-});
-
-module.exports = mongoose.model("Reward", rewardSchema);
