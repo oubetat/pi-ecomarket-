ef50ca59dffc778ae89c545d7db9cc23020d465c
Ameur <oubetat.1@gmail.com>
2026-04-02 21:13:33 +0100

    Delete user.js

--- user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  points: { type: Number, default: 0 },
-  level: { type: Number, default: 1 }
-});
-
-module.exports = mongoose.model('User', userSchema);
